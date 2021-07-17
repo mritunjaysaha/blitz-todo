d1724aeecb32489aa3ccae65294c3bd554ee7392
Mritunjay Saha <mritunjaysaha17@gmail.com>
2021-07-17 19:38:40 +0530

    removed boiler plate code

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -7,7 +7,7 @@ import {
   ErrorFallbackProps,
   useQueryErrorResetBoundary,
 } from "blitz"
-import LoginForm from "app/auth/components/LoginForm"
+// import LoginForm from "app/auth/components/LoginForm"
 
 export default function App({ Component, pageProps }: AppProps) {
   const getLayout = Component.getLayout || ((page) => page)
@@ -24,7 +24,10 @@ export default function App({ Component, pageProps }: AppProps) {
 
 function RootErrorFallback({ error, resetErrorBoundary }: ErrorFallbackProps) {
   if (error instanceof AuthenticationError) {
-    return <LoginForm onSuccess={resetErrorBoundary} />
+    return (
+      // <LoginForm onSuccess={resetErrorBoundary} />
+      <>sadas</>
+    )
   } else if (error instanceof AuthorizationError) {
     return (
       <ErrorComponent
