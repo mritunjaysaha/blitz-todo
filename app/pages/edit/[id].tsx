60f6082519440e3cb43ff3bbd47276c2472acc93
Mritunjay Saha <mritunjaysaha17@gmail.com>
2021-07-18 10:34:57 +0530

    added forms

--- app/pages/edit/[id].tsx
+++ app/pages/edit/[id].tsx
@@ -1,11 +1,15 @@
-import { BlitzPage } from "blitz"
+import { BlitzPage, useParam } from "blitz"
 import { Form } from "../../core/components/Form"
 const EditPage: BlitzPage = () => {
+  const id = useParam("id")
+
   return (
     <div>
-      <Form url={`http://localhost:8000/api/todo${id}`} method="PUT" />
+      <Form url={`http://localhost:8000/api/todo/${id}`} method="PUT" />
     </div>
   )
 }
+
+EditPage.suppressFirstRenderFlicker = true
 
 export default EditPage
